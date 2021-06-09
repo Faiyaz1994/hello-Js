ddaf1e5bee5718e52781c0477f1d4a36e34719ea
faiyaz1994 <faiyaz1994@gmail.com>
2021-06-09 09:35:48 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,20 @@
+//console.log(8);
+var bananaPrice=5;
+var bananaPrice=-6;
+//console.log(bananaPrice);
+
+
+//confusion
+var number1=15.35;
+var number2=25;
+
+var numbers = [6,7,8,1,2,3,4,5];
+
+var result = numbers.slice(1,3);
+
+//console.log(result)
+
+
+var a=[1, 5, 8];
+var b= a.indexOf(1);
+//console.log(b);
